0b66cc01e8f4adfcab6fc15ac6d1d93ecaef197a
Alex Contento <github@senorcontento.com>
2019-03-08 15:11:38 -0500

    Got basic encryption/decryption. Now just for the async response.

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ var port = 8080;
 
 var http = require('http');
 var dt = require('./server_modules/date');
+var gpg = require('gpg');
+var fs = require("fs");
 
 http.createServer(
   function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('Query: ' + getQuery(request, "hello"));
+
+    encrypt();
+    decrypt();
+
     response.end(); // End's Response
     //console.log('Date: ' + dt.myDateTime());
   }
@@ -17,4 +23,37 @@ http.createServer(
 
 function getQuery(request, query) {
   return request.url;
+}
+
+function encrypt() {
+  var mysecret = 'Hello World';
+  var args = [
+    '--default-key', 'github',
+    '--recipient', 'github',
+    '--armor',
+    '--trust-model', 'always'
+  ];
+  gpg.encrypt(mysecret, args, function(err, data){
+    //console.log(data);
+    fs.writeFile("temp.txt", data, function(err, data) {
+      if (err) console.log(err);
+        console.log("Successfully Written to File.");
+      });
+  });
+}
+
+function decrypt() {
+  var args = [
+    '--default-key', 'github',
+    '--recipient', 'github',
+    '--armor',
+    '--trust-model', 'always'
+  ];
+
+  fs.readFile("temp.txt", function(err, buf) {
+    filecontents = buf.toString();
+    gpg.decrypt(filecontents, args, function(err, contents){
+      console.log(contents.toString());
+    });
+  });
 }
