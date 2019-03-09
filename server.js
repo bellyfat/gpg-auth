828dad57baad4b4c55efe4b242da017446f12bcd
Alex Contento <github@senorcontento.com>
2019-03-08 21:30:17 -0500

    Given Difficulty of Putting an encrypted message as a GET request (I am going to add post too, but, I am still allowing GET), I added the ability to autodetect if a string is base64 or not.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // https://eloquentjavascript.net/11_async.html - Very Useful for Conceptualizing ASync Operations
 // https://blog.risingstack.com/node-hero-async-programming-in-node-js/ - Also Helps When You Just Don't Get It
 
-var encrypted_demo = "\n-----BEGIN PGP MESSAGE-----\n\nhQIMA6xagI2XC4DZAQ/+IkPHAh8yFFXzST3YleWVwFSYosBkMb3L1/nDIVvchhmB\nWcskWtNUmoGsNbMXOzTFwLYQwxDh1MSPT7uYmVft12QAnXO6yLxnxd7/NUkWNMQ+\nGc2oaUuCqP5gEBKVT8n5FDUfSV3nHYpST+KuKFRBi51BzZl9yQfXSzBbhAG5c/Sj\n2VHLMY5sYcxcH9xMY2lh0t9GWnJ5olXpnD+DemoVDYUDy2J7FSO2Xm3vg8l4qgqH\nkKE5zI7BMlOJdgWhSdPV5qppTiPD2k5G5LcOJlS3QWp654+9Ct9OfEUjOR/asnrn\nw8VN0o56ZSZh2jrKGXvwRxp80pB/ExLN7MR4eA9Ak4afHAefv1LJlb2lAgNc2Ci4\nttt14LaeD2TUSSTeQ54gU6hrXHkUkAd+uWu/P0rbL2AbSOl3mEX0/ArOVyJM1LDF\ncpW6n+uqywpFTZ/BVOVSj1UoOeQFQb/K//DP3YjOF/XokIUsjQMOXaR9LL+nxrWY\ni1Xt80e7kmgFAnbThX/BPIWFE7anHFfyycICW8L46tKDlKtLyrdw46mHScJzo553\npRL7jlUhVsMEdDyxongOONxliYlTuuDou8mB0ATZ+65W1unW5AEidf+sLvwlLyGH\ncj1H1vn5MxB+Cd+VUYvEf+dhTNbhyXg/nz04NGSzzVQXJzJNME8RlLuHlNCAdOLS\nQwECLWf19yLuC+Xj67HRvm0JMJSFtHlWpVXu9RN5uhIrzjGxAaPkHBIHyqv1F+Fd\n6t2/viNoNHtzetk1HHGA5FWCZsQ=\n=AoA8\n-----END PGP MESSAGE-----";
+var encrypted_demo = "vpTiPD2k5G5LcOJlS3QWp654+9Ct9OfEUjOR/asnrn\nw8VN0o56ZSZh2jrKGXvwRxp80pB/ExLN7MR4eA9Ak4afHAefv1LJlb2lAgNc2Ci4\nttt14LaeD2TUSSTeQ54gU6hrXHkUkAd+uWu/P0rbL2AbSOl3mEX0/ArOVyJM1LDF\ncpW6n+uqywpFTZ/BVOVSj1UoOeQFQb/K//DP3YjOF/XokIUsjQMOXaR9LL+nxrWY\ni1Xt80e7kmgFAnbThX/BPIWFE7anHFfyycICW8L46tKDlKtLyrdw46mHScJzo553\npRL7jlUhVsMEdDyxongOONxliYlTuuDou8mB0ATZ+65W1unW5AEidf+sLvwlLyGH\ncj1H1vn5MxB+Cd+VUYvEf+dhTNbhyXg/nz04NGSzzVQXJzJNME8RlLuHlNCAdOLS\nQwECLWf19yLuC+Xj67HRvm0JMJSFtHlWpVXu9RN5uhIrzjGxAaPkHBIHyqv1F+Fd\n6t2/viNoNHtzetk1HHGA5FWCZsQ=\n=AoA8\n-----END PGP MESSAGE-----";
 
 var host = 'localhost'; // '0.0.0.0' for all interfaces
 var port = 8080;
@@ -42,24 +42,34 @@ http.createServer(
   }
 ).listen(port, host);
 
+function buildJSON(key, object) {
+  var json = new Object()
+  json[key] = object;
+  return json;
+}
+
 function failme(response, error) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write(error);
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.write(JSON.stringify(buildJSON("error", error)));
   response.end(); // End Response
 }
 
 function encrypt(response, request) {
-  cryptography.encrypt(getQuery(request, "key"), getQuery(request, "message"), function(data) {
+  //console.log("Base64: " + isBase64(getQuery(request, "message")));
+  var message = isBase64(getQuery(request, "message")) ? getQuery(request, "message") : Buffer.from(getQuery(request, "message"), 'base64');
+  cryptography.encrypt(getQuery(request, "key"), message, function(data) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(data);
+    response.write(JSON.stringify(buildJSON("response", data)));
     response.end(); // End Response
   });
 }
 
 function decrypt(response, request) {
-  cryptography.decrypt(getQuery(request, "key"), getQuery(request, "message"), function(data) {
+  var message = isBase64(getQuery(request, "message")) ? getQuery(request, "message") : Buffer.from(getQuery(request, "message"), 'base64');
+  cryptography.decrypt(getQuery(request, "key"), message, function(data) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(data.toString());
+    //response.write(Buffer.from(getQuery(request, "message"), 'base64'));
+    response.write(JSON.stringify(buildJSON("response", data)));
     response.end(); // End Response
   });
 }
@@ -78,6 +88,11 @@ function verify(response, request) {
     response.write(data.toString());
     response.end(); // End Response
   });
+}
+
+function isBase64(bytes) {
+  // https://stackoverflow.com/a/48770228/6828099
+  return Buffer.from(bytes, 'base64').toString('base64') !== bytes;
 }
 
 function getQuery(request, query) {
